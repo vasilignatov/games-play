e4b9a899e907c03df69badeec2b34600ab67f846
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-02 20:51:02 +0200

    Create Home controller

--- src/App.js
+++ src/App.js
@@ -1,86 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; +
+
+import Header from './components/Header.js';
+import Home from './components/Home.js';
 
 function App() {
   return (
     <div id="box">
 
-      <header>
-        {/* <!-- Navigation --> */}
-        <h1><a className="home" href="#">GamesPlay</a></h1>
-        <nav>
-          <a href="#">All games</a>
-          {/* <!-- Logged-in users --> */}
-          <div id="user">
-            <a href="#">Create Game</a>
-            <a href="#">Logout</a>
-          </div>
-          {/* <!-- Guest users --> */}
-          <div id="guest">
-            <a href="#">Login</a>
-            <a href="#">Register</a>
-          </div>
-        </nav>
-      </header>
+      <Header />
 
-      {/* <!-- Main Content --> */}
+      <Home />
+
       <main id="main-content">
       </main>
 
       {/* <!--Home Page--> */}
-      <section id="welcome-world">
 
-        <div className="welcome-message">
-          <h2>ALL new games are</h2>
-          <h3>Only in GamesPlay</h3>
-        </div>
-        <img src="./images/four_slider_img01.png" alt="hero" />
-
-        <div id="home-page">
-          <h1>Latest Games</h1>
-
-          {/* <!-- Display div: with information about every game (if any) --> */}
-          <div className="game">
-            <div className="image-wrap">
-              <img src="./images/CoverFire.png" />
-            </div>
-            <h3>Cover Fire</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-          <div className="game">
-            <div className="image-wrap">
-              <img src="./images/ZombieLang.png" />
-            </div>
-            <h3>Zombie Lang</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-          <div className="game">
-            <div className="image-wrap">
-              <img src="./images/MineCraft.png" />
-            </div>
-            <h3>MineCraft</h3>
-            <div className="rating">
-              <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-            </div>
-            <div className="data-buttons">
-              <a href="#" className="btn details-btn">Details</a>
-            </div>
-          </div>
-
-          {/* <!-- Display paragraph: If there is no games  --> */}
-          <p className="no-articles">No games yet</p>
-        </div>
-      </section>
 
       {/* <!-- Login Page ( Only for Guest users ) --> */}
       <section id="login-page" className="auth">
@@ -238,7 +173,7 @@ function App() {
         {/* <!-- Display div: with information about every game (if any) --> */}
         <div className="allGames">
           <div className="allGames-info">
-            <img src="./images/avatar-1.jpg" />
+            <img src="/images/avatar-1.jpg" />
             <h6>Action</h6>
             <h2>Cover Fire</h2>
             <a href="#" className="details-button">Details</a>
@@ -247,7 +182,7 @@ function App() {
         </div>
         <div className="allGames">
           <div className="allGames-info">
-            <img src="./images/avatar-1.jpg" />
+            <img src="/images/avatar-1.jpg" />
             <h6>Action</h6>
             <h2>Zombie lang</h2>
             <a href="#" className="details-button">Details</a>
@@ -256,7 +191,7 @@ function App() {
         </div>
         <div className="allGames">
           <div className="allGames-info">
-            <img src="./images/avatar-1.jpg" />
+            <img src="/images/avatar-1.jpg" />
             <h6>Action</h6>
             <h2>MineCraft</h2>
             <a href="#" className="details-button">Details</a>
