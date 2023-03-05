d0ccbbfb70fa9ca68d7cc2c8739822442f413ec6
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-05 13:20:32 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import Header from './components/Header.js';
 import Home from './components/Home.js';
+import Catalog from './components/Catalog.js';
+import Create from './components/Create.js';
+import Details from './components/Details.js';
+import Edit from './components/Edit.js';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
 
 function App() {
+
+  const routes = {
+    '/home': Home,
+    '/games': Catalog,
+    '/create': Create,
+  }
+
   return (
     <div id="box">
 
