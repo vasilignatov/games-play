fee81c078fe50ceb6192c859416fc656649142a2
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-03-14 21:15:57 +0200

    Add react routing exercise

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
-import CatalogGameCard from './CatalogGamesCard.js';
+import React, { Suspense, useEffect, useState } from 'react';
+
+// import CatalogGameCard from './CatalogGamesCard.js';f
 import * as gameService from '../services/gameService.js';
-import React from 'react';
-const Catalog = ({ navigationChangeHandler }) => {
+const GameCard = React.lazy(() => import('./CatalogGamesCard'));
+
+const Catalog = () => {
 
     const [games, setGames] = useState([]);
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        setLoading(true);
+        // setLoading(true);
         setTimeout(() => {
             gameService.getAll()
                 .then(setGames);
-            setLoading(false);
+            // setLoading(false);
         }, 1000);
     }, []);
 
@@ -21,16 +23,23 @@ const Catalog = ({ navigationChangeHandler }) => {
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {
-                loading
-                    ? <h2 style={{ color: 'white', textAlign: 'center' }}>Loading...</h2>
-                    : games.length > 0
-                        ? games.map(g => <CatalogGameCard key={g._id} game={g} navigationChangeHandler={navigationChangeHandler} />)
-                        : <h3 className="no-articles">No games yet</h3>
-            }
+            <Suspense fallback={<h2 style={{ backgroundColor: 'white', color: 'black' }}>Loading...</h2>}>
+                {games.length > 0
+                    ? games.map(g => <GameCard key={g._id} game={g} />)
+                    : <h3 className="no-articles">No games yet</h3>
+                }
 
+                {/* {
+                    loading
+                        ? <h2 style={{ color: 'white', textAlign: 'center' }}>Loading...</h2>
+                        : games.length > 0
+                            ? games.map(g => <CatalogGameCard key={g._id} game={g} navigationChangeHandler={navigationChangeHandler}1 />)
+                            f: <h3 className="no-articles">No games yet</h3>
+                } */}
+
+            </Suspense>
         </section>
-    )
+    );
 }
 
 export default Catalog;
--- src/components/CatalogGamesCard.js
+++ src/components/CatalogGamesCard.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+// const ratingCalculator = React.lazy(() => import('../utils/ratingCalculator.js'));
+
 const CatalogGameCard = ({ game }) => {
+
+
     return (
         <div className="allGames">
             <div className="allGames-info">
                 <img src={game.imageUrl} />
                 <h6>{game.category}</h6>
                 <h2>{game.title}</h2>
+                {/* <p>Rating: {ratingCalculator()}</p> */}
                 <Link to={`/games/${game._id}`} className="details-button">Details</Link>
             </div>
         </div>
